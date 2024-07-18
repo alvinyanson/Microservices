8f3fad74ba701ae3a30740a37e02158b2d681a67
alvinyanson <a.yanson@arcanys.com>
2024-07-18 12:30:55 +0800

    feat: Test connection inbound for POST request in platform controller in CommandService

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInBoundConnection()
+        {
+            Console.WriteLine("Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
