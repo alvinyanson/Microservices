409812b3eacef1efa25cbc5c92a4772091679395
alvinyanson <a.yanson@arcanys.com>
2024-07-19 10:04:36 +0800

    build: Added connection string for prod sql server

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,18 +19,30 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        private IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseInMemoryDatabase("InMem"));
+            if(_env.IsProduction()) {
+                Console.WriteLine("Using SQL Server Db");
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else {
+                Console.WriteLine("Using InMemory Db");
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseInMemoryDatabase("InMem"));                
+            }
+
 
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
