ca8741d59d8ebdd343073f2ecdbdad4883250e15
alvinyanson <a.yanson@arcanys.com>
2024-07-19 14:53:49 +0800

    feat: Added controller action for command service

--- CommandService/Controllers/PlatformsController.cs
+++ CommandService/Controllers/PlatformsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CommandService.Controllers
 {
@@ -7,9 +12,13 @@ namespace CommandService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repo, IMapper mapper)
         {
-            
+            _repo = repo;
+            _mapper = mapper;
         }
 
         [HttpPost]
@@ -18,6 +27,16 @@ namespace CommandService.Controllers
             Console.WriteLine("Inbound POST # Command Service");
 
             return Ok("Inbound test of from Platforms Controller");
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("Getting platforms from CommandService...");
+
+            var platformItems = _repo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
     }
 }
--- CommandService/Dtos/CommandCreateDto.cs
+++ CommandService/Dtos/CommandCreateDto.cs
@@ -4,10 +4,6 @@ namespace CommandService.Dtos
 {
     public class CommandCreateDto
     {
-        [Key]
-        [Required]
-        public int Id { get; set; }
-
         [Required]
         public string HowTo { get; set; }
 
